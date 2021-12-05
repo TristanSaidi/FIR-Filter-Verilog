ab9de7137870bae92dcdccea37454922fa911ba1
Brad Sears <bws2121@columbia.edu>
2021-12-04 19:49:55 -0500

    Edited Makefile to run convert.py after simulation.

--- qsim_rtl/fir_filter/convert.py
+++ qsim_rtl/fir_filter/convert.py
@@ -1,21 +1,26 @@
 import os, sys
 import re, math
 
+def twos_comp(val, bits):
+	if (val & (1 << (bits - 1))) != 0:
+		val = val - (1 << bits)
+	return val 
+
 def main():
 	fp	= open(sys.argv[1], "r")
 	lines	= fp.readlines()
 	fp.close()
 	inputs	= list()
 	for line in lines:
-		line = line.strip().split()
+		line = line.strip()
 		inputs.append(line)
 	fp	= open(sys.argv[2], "w")
 	for line in inputs:
-		value	= int(line, 2)
+		value	= twos_comp(int(line,2), len(line))
 		fp.write("{}\n".format(value))
 	fp.close()
 	return
 
 if __name__ == "__main__":
-	main
+	main()
 	sys.exit(0)
