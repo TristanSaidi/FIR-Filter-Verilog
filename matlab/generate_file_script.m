5103c7ea91ca52061ac21ad3747059f68f82f8f9
tls2160 <tls2160@cadpc14.ee.columbia.edu>
2021-12-07 18:19:14 -0500

    Debugging

--- matlab/generate_file_script.m
+++ matlab/generate_file_script.m
@@ -3,13 +3,16 @@ inputs = fopen('../qsim_rtl/fir_filter/reports/inputs.rpt','r');
 outputs = fopen('../qsim_rtl/fir_filter/reports/outputs.rpt','r');
 
 
-b = fscanf(coefficients,'%d');
-x = fscanf(inputs,'%d');
-tb_out = fscanf(outputs,'%d');
+b = int64(fscanf(coefficients,'%i'));
+x = int64(fscanf(inputs,'%i'));
+tb_out = fscanf(outputs,'%i');
 
 %convolved signal
-y = filter_function(x,b);
+y = int64(filter_function(x,b));
 
+x_conv = x/(2^16);
+b_conv = b/(2^16);
+y_conv = filter_function(x_conv,b_conv);
 
 tb_bin_out = dec2bin(tb_out); %truncated binary from tb
 out = dec2bin(y);
@@ -19,14 +22,19 @@ y_bin_out = out(:,26:41); %truncated binary from matlab
 out = binary2decimal(y_bin_out);
 
 
-exact_expected_value = y/(2^33); %exact value
-truncated_expected_value = out/(2^10); %expected decimal w/ truncation
-tb_out_value = tb_out/(2^10);  %observed decimal w/ truncation
+exact_expected_value = y/(2^32); %exact value
+truncated_expected_value = out/(2^9); %expected decimal w/ truncation
+tb_out_value = tb_out/(2^9);  %observed decimal w/ truncation
 
+sum = 0;
 error_count = 0;
-for i=1:length(tb_out)-2
-    if(outdec(i) ~= tb_out(i+2))
+for i=1:length(tb_out_value)-2
+    sum = sum + (tb_out_value(i) - exact_expected_value(i))^2;
+    if(truncated_expected_value(i) ~= tb_out_value(i+2))
         error_count = error_count+1;
     end
 end
+
+% RMSE = sqrt(sum/(length(tb_out_value)-2));
+% NRMSE = RMSE/(max(tb_out_value)-min(tb_out_value));
 disp("Error count: "+error_count);
---
+++ matlab/untitled.m
@@ -0,0 +1,7 @@
+add=0;
+cur=15361;
+for i=1:22
+    add = add+cur;
+    add=add*2;
+end
+disp(add)
